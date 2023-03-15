6f2e7de403f1261e17dcabf65bd02279e1362431
Clément <clement2104.boillot@gmail.com>
2023-03-15 18:41:31 +0100

    :recycle: Change /all-images page to be server side generated

--- src/server.js
+++ src/server.js
@@ -18,6 +18,49 @@ server.on('request', (req, res) => {
     return;
   }
 
+  if (req.url === '/all-images') {
+    let HTMLPage = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../style/global.css">
+  <title>Clément Boillot - All Images</title>
+</head>
+<body>
+  <main>
+    <h1>Mur d'images</h1>
+    <div class="buttons">
+      <a href="/" class="button">accueil</a>
+    </div>
+    <div class="images-container">`
+
+    // for (let i = 1; fs.existsSync(`/images/image${i}.jpg`); i++)
+    fs.readdirSync("./images").sort().forEach(img => {
+      if (img.endsWith("_small.jpg"))
+        return;
+
+      const index = parseInt(
+        img.replace(".jpg", "")
+           .replace("image", "")
+      );
+      HTMLPage += `
+      <a href="/image?id=${index}" class="img">
+        <img src="../images/image${index}_small.jpg">
+      </a>`;
+    })
+
+    HTMLPage += `
+    </div>
+  </main>
+</body>
+</html>`;
+    res.end(HTMLPage);
+    return;
+  }
+
   let filename = `.${req.url}`
 
   if (fs.existsSync(`./pages${req.url}.html`))
