18166b4bab2dc9ed4522c9b73da9089791662813
Clément <clement2104.boillot@gmail.com>
2023-03-16 14:17:37 +0100

    :lipstick: Change language to fr

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ server.on('request', (req, res) => {
     )
 
     let HTMLPage = `<!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -68,7 +68,7 @@ server.on('request', (req, res) => {
   if (req.url === '/all-images') {
     let HTMLPage = `
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
