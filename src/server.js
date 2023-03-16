0c43d84811eb7f413e70c7d5478fd9e1a3030f9f
Clément <clement2104.boillot@gmail.com>
2023-03-16 18:39:16 +0100

    :lipstick: Add navigation buttons

--- src/server.js
+++ src/server.js
@@ -101,7 +101,7 @@ server.on('request', (req, res) => {
         <body>
           <main>
             <h1>Mur d'images</h1>
-            <div class="buttons">
+            <div class="buttons center">
               <a href="/" class="button">accueil</a>
               <a href="/image-description" class="button">Ajouter une description</a>
             </div>
@@ -174,12 +174,16 @@ server.on('request', (req, res) => {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="../style/global.css">
-            <title>Clément Boillot - Error 404</title>
+            <link rel="stylesheet" href="./style/global.css">
+            <title>Clément Boillot - Description ajoutée</title>
           </head>
           <body>
             <main>
-              <h2>Commentaire ajouté!</h2>
+              <h1>Commentaire ajouté!</h1>
+              <div class="buttons center">
+                <a href="/" class="button">Accueil</a>
+                <a href="/all-images" class="button">Mur d'images</a>
+              </div>
               <p>Le commentaire "${description}" a été rajouté à l'image numéro ${index}.</p>        
             </main>
           </body>
