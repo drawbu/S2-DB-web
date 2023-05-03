4750def7be9fb39783aef8c07254a602f0131888
Clément <clement2104.boillot@gmail.com>
2023-05-03 09:51:01 +0200

    :bug: Fill the window if the client can't scroll

--- public/mur.js
+++ public/mur.js
@@ -17,22 +17,35 @@ layoutButton.addEventListener('click', () => {
 
 // Load more images when the user is close to the bottom of the page
 window.addEventListener('scroll', () => {
-  const images = imagesGrid.querySelectorAll('a.image');
-  // If the client view is at less than 100 pixels from the bottom of the page
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
-    const lastImage = images[images.length - 1];
-    const imageId = parseInt(
-      lastImage.href.replace('/image', '')
-                    .replace(window.location.origin, '')
-    );
-    if (isNaN(imageId) || imageId >= 53) {
-      return;
-    }
-    const nextImage = lastImage.cloneNode(true);
-    const imageThumbnail = nextImage.querySelector('img')
-    nextImage.href = `/image${imageId + 1}`;
-    imageThumbnail.alt = `Image ${imageId + 1}`;
-    imageThumbnail.src = `./public/images/image${imageId + 1}_small.jpg`
-    imagesGrid.appendChild(nextImage);
+    addNextImage();
   }
 });
+
+// Fill the window if the client can't scroll
+const interval = setInterval(() => {
+  if (window.innerHeight === document.body.offsetHeight) {
+    addNextImage();
+  }
+}, 100);
+
+function addNextImage() {
+  const images = imagesGrid.querySelectorAll('a.image');
+  const lastImage = images[images.length - 1];
+  const imageId = parseInt(
+    lastImage.href.replace('/image', '')
+                  .replace(window.location.origin, '')
+  );
+  if (isNaN(imageId) || imageId >= 53) {
+    clearInterval(interval);
+    return;
+  }
+  const nextImage = lastImage.cloneNode(true);
+  const imageThumbnail = nextImage.querySelector('img');
+  nextImage.href = `/image${imageId + 1}`;
+  imageThumbnail.alt = `Image ${imageId + 1}`;
+  imageThumbnail.src = `./public/images/image${imageId + 1}_small.jpg`;
+  imagesGrid.appendChild(nextImage);
+}
+
+
